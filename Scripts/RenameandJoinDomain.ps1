98be33fed3e256f9a0920d370ca09fa2f39ad22a
Anthony Sheculski <anthony.sheculski@clearesult.com>
2022-01-04 16:07:21 -0500

    new scripts

---
+++ Scripts/RenameandJoinDomain.ps1
@@ -0,0 +1,9 @@
+New-ADComputer -Name "Server02" -AccountPassword (ConvertTo-SecureString -String 'TempJoinPA$$' -AsPlainText -Force)
+
+# Then this command is run from `Server02` which is not yet domain-joined:
+
+$joinCred = New-Object pscredential -ArgumentList ([pscustomobject]@{
+    UserName = $null
+    Password = (ConvertTo-SecureString -String 'TempJoinPA$$' -AsPlainText -Force)[0]
+})
+Add-Computer -Domain "Domain03" -Options UnsecuredJoin,PasswordPass -Credential $joinCred
