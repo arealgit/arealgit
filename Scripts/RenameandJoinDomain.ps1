a0763e2753fab3c62386cc4ed27e52bc3ef929b0
Anthony Sheculski <anthony.sheculski@clearesult.com>
2022-01-04 16:09:43 -0500

    test

--- Scripts/RenameandJoinDomain.ps1
+++ Scripts/RenameandJoinDomain.ps1
@@ -7,3 +7,18 @@ $joinCred = New-Object pscredential -ArgumentList ([pscustomobject]@{
     Password = (ConvertTo-SecureString -String 'TempJoinPA$$' -AsPlainText -Force)[0]
 })
 Add-Computer -Domain "Domain03" -Options UnsecuredJoin,PasswordPass -Credential $joinCred
+
+
+
+
+
+###########
+
+
+
+$domain = "domain.local"
+$password = "Passw@rd" | ConvertTo-SecureString -asPlainText -Force
+$username = "$domain\Administrator"
+$hostname=hostname
+$credential = New-Object System.Management.Automation.PSCredential($username,$password)
+Add-Computer -DomainName $domain -ComputerName $hostname -NewName alrootca -Credential $credential -Restart
