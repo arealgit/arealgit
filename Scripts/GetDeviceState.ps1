67cbd4dfa7519641a6787b745c64d3030dc39d32
Anthony Sheculski <anthony.sheculski@clearesult.com>
2022-01-12 10:54:25 -0500

    My first txt file parsing script

--- Scripts/GetDeviceState.ps1
+++ Scripts/GetDeviceState.ps1
@@ -1,5 +1,3 @@
-function Get-DeviceState
-
 <#
 .SYNOPSIS
 Runs dsregcmd with the status switch and parses the output to return a hash table of the Device State.
@@ -8,39 +6,35 @@ Runs dsregcmd with the status switch and parses the output to return a hash tabl
 Runs dsregcmd with the status switch and parses the string output to return a dsregcmd object.
 
 .EXAMPLE
-GetDsRegCmdStatus | Where-Object {$_.'Device State'.AzureAdJoined -eq 'Yes'}
+Get-DeviceState | Where-Object {$_.AzureAdJoined -eq 'True'}
 #>
 
-$DeviceState = [Ordered]@{}
-$Status = dsregcmd /status
+function Get-DeviceState {
+    [PSCustomObject]$DeviceState
+    $Status = dsregcmd /status
+    $AADjoined = $Status | Select-String "AzureAdJoined"
+    $EnterpriseJoined = $Status | Select-String "EnterpriseJoined"
+    $DomainJoined = $Status | Select-String "DomainJoined"
+    $DomainName = $Status | Select-String "DomainName" | Out-String
+    $DeviceName = $Status | Select-String "Device Name" | Out-String
 
-$DeviceName = $Status | Select-String "Device Name"
-$AADjoined = $Status | Select-String "AzureAdJoined"
-$EnterpriseJoined = $Status | Select-String "EnterpriseJoined"
-$DomainJoined = $Status | Select-String "DomainJoined"
-if($DomainJoined) {
-    $DomainName = $Status | Select-String "DomainName"
-    $DomainName = $DomainName.Substring(0,10)
-}
+    if($AADjoined -match "YES") {
+        [bool]$DeviceState.AzureAdJoined = 1
+        } else {[bool]$DeviceState.AzureAdJoined = 0}
 
+    if($EnterpriseJoined -match "YES") {
+        [bool]$DeviceState.EnterpriseJoined = 1
+        } else {[bool]$DeviceState.EnterpriseJoined = 0}
 
-if($AADjoined -match "YES") {
-    [bool]$DeviceState.AzureAdJoined = 1
-    } else {[bool]$DeviceState.AzureAdJoined = 0}
-
-if($EnterpriseJoined -match "YES") {
-    [bool]$DeviceState.EnterpriseJoined = 1
-    } else {[bool]$DeviceState.EnterpriseJoined = 0}
-
-if($DomainJoined -match "YES") {
-    [bool]$DeviceState.DomainJoined = 1
-    } else {
+    if($DomainJoined -match "YES") {
+        [bool]$DeviceState.DomainJoined = 1
+        } else {
         [bool]$DeviceState.DomainJoined = 0
         }
 
-if($DomainName) {
-    [string]$DeviceState.DomainName = 
+    if($DomainName) {
+        [string]$DeviceState.DomainName = $DomainName.Trim().TrimStart("DomainName : ")
+        }
+
+    $DeviceState.DeviceName = $DeviceName.Trim().TrimStart('Device Name : ')
     }
-
-
-
