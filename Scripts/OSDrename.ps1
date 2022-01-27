0419bd1e6c6a31c0eb7bb3b5e8b0d4ce172381c5
Anthony Sheculski <anthony.sheculski@clearesult.com>
2022-01-27 16:08:46 -0500

    updated osdrename script

--- Scripts/OSDrename.ps1
+++ Scripts/OSDrename.ps1
@@ -1,4 +1,21 @@
 $SerialNumber = (Get-WmiObject -Class Win32_BIOS | Select-Object SerialNumber).SerialNumber
-$OSDComputerName = "Contoso-" + $SerialNumber
+
+$Chassis = Get-WmiObject win32_systemenclosure | select-object -expandproperty chassistypes
+$ChassisCode = ""
+
+switch ($Chassis)
+{
+2 { $ChassisCode = "W"}
+3 { $ChassisCode = "W"}
+4 { $ChassisCode = "W"}
+5 { $ChassisCode = "W"}
+6 { $ChassisCode = "W"}
+7 { $ChassisCode = "W"}
+8 { $ChassisCode = "L"}
+9 { $ChassisCode = "L"}
+10 { $ChassisCode = "L"}
+}
+
+$OSDComputerName = "CR-" + $ChassisCode + $SerialNumber
 
 Rename-Computer -ComputerName $env:computername -NewName $OSDComputerName
