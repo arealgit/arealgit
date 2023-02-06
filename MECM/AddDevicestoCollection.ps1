0e37ec0ee358d60d04abd0d28ece6e18e8005ded
arealgit <44850306+arealgit@users.noreply.github.com>
2023-02-05 22:18:03 -0500

    Delete AddDevicestoCollection.ps1

--- MECM/AddDevicestoCollection.ps1
+++
@@ -1,5 +0,0 @@
-
-$CollectionID = "PKISB0015B"
-$Computers = Get-Content "C:\Users\anthony\Desktop\computers.txt" 
-
-$Computers| foreach { Add-CMDeviceCollectionDirectMembershipRule -CollectionId PKISB0015B -ResourceID (Get-CMDevice -Name $_).ResourceID }
