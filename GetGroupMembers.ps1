e35b6e7091dfb68a10335a07e26b79c0f0d78ddc
arealgit <44850306+arealgit@users.noreply.github.com>
2023-02-05 22:07:13 -0500

    Delete GetGroupMembers.ps1
    This file was added to my PowerShell repository

--- GetGroupMembers.ps1
+++
@@ -1,6 +0,0 @@
-
-$ADGroup = Read-Host -Prompt 'Input name of group'
-$ADGroupMembers = Get-AdgroupMember $ADgroup
-foreach ($member in $ADGRoupMembers) {get-aduser $member.samaccountname -properties displayname | select displayname,samaccountname,name}
-
-
